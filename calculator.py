fdae667f6ca5dfa5a328595aeaa16885f4768a58
Hackbright Student <user@mini-007.(none)>
2014-02-05 15:43:46 -0800

    Added recovery messages for incorrect input

--- calculator.py
+++ calculator.py
@@ -1,4 +1,5 @@
-import arithmetic.py
+import string
+from arithmetic import *
 
 # take user's input
 # tokenize input
@@ -8,26 +9,58 @@ import arithmetic.py
 while True:
     user_input = raw_input("> ")
 
-    tokens = input.split(" ")
+    tokens = string.split(user_input)
 
     if tokens[0] == "+":
-        pass
+        if len(tokens) != 3:
+            print "Please enter two numbers to add. For example:"
+            print "+ 1 2"
+        else:
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print add(tokens[1], tokens[2])
     elif tokens[0] == "-":
-        pass
+        if len(tokens) != 3:
+            print "Please enter two numbers to subtract. For example:"
+            print "- 3 1"
+        else:
+            pass
     elif tokens[0] == "*":
-        pass
+        if len(tokens) != 3:
+            print "Please enter two numbers to multiply. For example:"
+            print "* 4 2"
+        else:
+            pass
     elif tokens[0] == "/":
-        pass
-    elif tokens[0] == "%":
-        pass
+        if len(tokens) != 3:
+            print "Please enter two numbers to divide. For example:"
+            print "/ 4 2"
+        else:
+            pass
     elif tokens[0] == "square":
-        pass
+        if len(tokens) != 2:
+            print "Please enter one number to square. For example:"
+            print "square 3"
+        else:
+            pass
     elif tokens[0] == "cube":
-        pass
+        if len(tokens) != 2:
+            print "Please enter one number to cube. For example:"
+            print "cube 2"
+        else:
+            pass
     elif tokens[0] == "power":
-        pass
+        if len(tokens) != 3:
+            print "Please enter exactly two numbers. For example:"
+            print "power 2 5"
+        else:
+            pass
     elif tokens[0] == "mod":
-        pass
+        if len(tokens) != 3:
+            print "Please enter exactly two numbers. For example:"
+            print "mod 5 3"
+        else:
+            pass
     elif tokens[0] == "q":
         exit()
     else:
