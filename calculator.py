d8a1a3f7f7bb01cf4f9a5ab94844b505d1a767ee
Hackbright Student <user@mini-007.(none)>
2014-02-05 15:47:54 -0800

    Called all relevant arithmetic functions

--- calculator.py
+++ calculator.py
@@ -1,9 +1,6 @@
 import string
 from arithmetic import *
 
-# take user's input
-# tokenize input
-# check to see if they input the right # of args for that function
 # perform calculation
 
 while True:
@@ -24,43 +21,55 @@ while True:
             print "Please enter two numbers to subtract. For example:"
             print "- 3 1"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print subtract(tokens[1], tokens[2])
     elif tokens[0] == "*":
         if len(tokens) != 3:
             print "Please enter two numbers to multiply. For example:"
             print "* 4 2"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print multiply(tokens[1], tokens[2])
     elif tokens[0] == "/":
         if len(tokens) != 3:
             print "Please enter two numbers to divide. For example:"
             print "/ 4 2"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print divide(tokens[1], tokens[2])
     elif tokens[0] == "square":
         if len(tokens) != 2:
             print "Please enter one number to square. For example:"
             print "square 3"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            print square(tokens[1])
     elif tokens[0] == "cube":
         if len(tokens) != 2:
             print "Please enter one number to cube. For example:"
             print "cube 2"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            print cube(tokens[1])
     elif tokens[0] == "power":
         if len(tokens) != 3:
             print "Please enter exactly two numbers. For example:"
             print "power 2 5"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print power(tokens[1], tokens[2])
     elif tokens[0] == "mod":
         if len(tokens) != 3:
             print "Please enter exactly two numbers. For example:"
             print "mod 5 3"
         else:
-            pass
+            tokens[1] = int(tokens[1])
+            tokens[2] = int(tokens[2])
+            print mod(tokens[1], tokens[2])
     elif tokens[0] == "q":
         exit()
     else:
