5761f72ec5af69fcea3fa0505951a78084d7d960
Hackbright Student <user@mini-007.(none)>
2014-02-05 16:40:08 -0800

    modified asmd to take multiple args

--- calculator.py
+++ calculator.py
@@ -1,7 +1,6 @@
 import string
 from arithmetic import *
 
-# perform calculation
 
 while True:
     user_input = raw_input("> ")
@@ -9,68 +8,77 @@ while True:
     tokens = string.split(user_input)
 
     if tokens[0] == "+":
-        if len(tokens) != 3:
-            print "Please enter two numbers to add. For example:"
-            print "+ 1 2"
+        if len(tokens) < 3:
+            print "Please enter at least two numbers to add. For example:   + 1 2"
         else:
-            tokens[1] = int(tokens[1])
-            tokens[2] = int(tokens[2])
-            print add(tokens[1], tokens[2])
+            tokens.pop(0)
+            answer = int(tokens.pop(0))
+            for i in tokens:
+                answer += int(i)
+            print answer
+
     elif tokens[0] == "-":
-        if len(tokens) != 3:
-            print "Please enter two numbers to subtract. For example:"
-            print "- 3 1"
+        if len(tokens) < 3:
+            print "Please enter at least two numbers to subtract. For example:   - 3 1"
         else:
-            tokens[1] = int(tokens[1])
-            tokens[2] = int(tokens[2])
-            print subtract(tokens[1], tokens[2])
+            tokens.pop(0)
+            answer = int(tokens.pop(0))
+            for i in tokens:
+                answer -= int(i)
+            print answer
+
     elif tokens[0] == "*":
-        if len(tokens) != 3:
-            print "Please enter two numbers to multiply. For example:"
-            print "* 4 2"
+        if len(tokens) < 3:
+            print "Please enter at least two numbers to multiply. For example:   * 4 2"
         else:
-            tokens[1] = int(tokens[1])
-            tokens[2] = int(tokens[2])
-            print multiply(tokens[1], tokens[2])
+            tokens.pop(0)
+            answer = int(tokens.pop(0))
+            for i in tokens:
+                answer *= int(i)
+            print answer
+
     elif tokens[0] == "/":
-        if len(tokens) != 3:
-            print "Please enter two numbers to divide. For example:"
-            print "/ 4 2"
+        if len(tokens) < 3:
+            print "Please enter at least two numbers to divide. For example:   / 4 2"
         else:
-            tokens[1] = int(tokens[1])
-            tokens[2] = int(tokens[2])
-            print divide(tokens[1], tokens[2])
+            tokens.pop(0)
+            answer = float(tokens.pop(0))
+            for i in tokens:
+                answer /= int(i)
+            print answer
+
     elif tokens[0] == "square":
         if len(tokens) != 2:
-            print "Please enter one number to square. For example:"
-            print "square 3"
+            print "Please enter one number to square. For example:   square 3"
         else:
             tokens[1] = int(tokens[1])
             print square(tokens[1])
+
     elif tokens[0] == "cube":
         if len(tokens) != 2:
-            print "Please enter one number to cube. For example:"
-            print "cube 2"
+            print "Please enter one number to cube. For example:   cube 2"
         else:
             tokens[1] = int(tokens[1])
             print cube(tokens[1])
+
     elif tokens[0] == "power":
         if len(tokens) != 3:
-            print "Please enter exactly two numbers. For example:"
-            print "power 2 5"
+            print "Please enter exactly two numbers. For example:   power 2 5"
         else:
             tokens[1] = int(tokens[1])
             tokens[2] = int(tokens[2])
             print power(tokens[1], tokens[2])
+
     elif tokens[0] == "mod":
         if len(tokens) != 3:
-            print "Please enter exactly two numbers. For example:"
-            print "mod 5 3"
+            print "Please enter exactly two numbers. For example:   mod 5 3"
         else:
             tokens[1] = int(tokens[1])
             tokens[2] = int(tokens[2])
             print mod(tokens[1], tokens[2])
+
     elif tokens[0] == "q":
         exit()
+
     else:
         print "Sorry, I'm not sure what that means. Please enter a valid argument."
